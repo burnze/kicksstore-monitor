33abb91d907bd551235253e3685b76fb360f45e5
burnze <52211699+burnze@users.noreply.github.com>
2019-08-06 23:22:01 +0200

    init

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ function compare(product) {
 function webhook(productTitle, productUrl, size) {
     const body = { 
         "username": "KicksStore Monitor",
-        "avatar_url": "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg",
         "embeds": [
             {
               "title": "Size restocked!",
